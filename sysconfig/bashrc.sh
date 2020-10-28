3f84a8f654b545a6d7b32028cee01c7b5b913154
Mohamadmehdi Kharatizadeh <info@ucoder.ir>
2020-10-28 16:04:21 +0330

    working on vnautilus

--- sysconfig/bashrc.sh
+++ sysconfig/bashrc.sh
@@ -345,6 +345,10 @@ function __vim() {
       extra_options="$extra_options --shm-size=$VIM_SHM_LIMIT"
     fi
 
+    if [ "$(which fzf)" != "" ]; then
+      extra_options="$extra_options -v $(which fzf):/usr/bin/fzf"
+    fi
+
     $DOCKER \
       run \
         -td \
@@ -437,7 +441,81 @@ function beautify() {
   fi
 }
 
+function vnautilus() {
+  DOCKER="docker"
+  if [ "$(groups | grep docker)" == "" ]; then
+    DOCKER="sudo docker"
+  fi
+
+  extra_options=""
+  if [ "$NAUTILUS_MEMORY_LIMIT" != "" ]; then
+    extra_options="$extra_options --memory=$NAUTILUS_MEMORY_LIMIT --oom-kill-disable"
+  fi
+  if [ "$NAUTILUS_SHM_LIMIT" != "" ]; then
+    extra_options="$extra_options --shm-size=$NAUTILUS_SHM_LIMIT"
+  fi
+
+  pulse_socket_file="$HOME/.config/pulse/nautilus.sock"
+  pactl load-module module-native-protocol-unix socket=$pulse_socket_file
+
+  $DOCKER run \
+    -td \
+    --name nautilus \
+    --rm \
+    --network host \
+    --hostname $(echo $HOSTNAME) \
+    -e PS_PREFIX=NAUTILUS \
+    -e GTK_IM_MODULE=ibus \
+    -e XMODIFIERS="@im=ibus" \
+    -e QT_IM_MODULE=ibus \
+    -e DISPLAY=$DISPLAY \
+    -e DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS \
+    -e PULSE_COOKIE=$HOME/.config/pulse/cookie \
+    -e PULSE_SERVER=$pulse_socket_file \
+    --mount type=bind,source=$HOME/.config/ibus,target=$HOME/.config/ibus \
+    --mount type=bind,source=$HOME/.cache/dconf,target=$HOME/.cache/dconf \
+    --mount type=bind,source=$HOME/.config/pulse,target=$HOME/.config/pulse \
+    --mount type=bind,source=$HOME/.dbus,target=$HOME/.dbus \
+    --mount type=bind,source=/dev/dri,target=/dev/dri \
+    --mount type=bind,source=/usr/lib,target=/usr/lib \
+    --mount type=bind,source=/usr/bin,target=/usr/bin \
+    --mount type=bind,source=/usr/share,target=/usr/share \
+    --mount type=bind,source=/etc/fonts,target=/etc/fonts \
+    --mount type=bind,source=/etc/pulse,target=/etc/pulse \
+    --mount type=bind,source=/tmp/tmux-$(id -u),target=/tmp/tmux-$(id -u) \
+    --mount type=bind,source=/var/cache/fontconfig,target=/var/cache/fontconfig \
+    --mount type=bind,source=/var/run/dbus,target=/var/run/dbus \
+    -v /etc/machine-id:/etc/machine-id \
+    -v /etc/os-release:/etc/os-release \
+    -v /etc/ld.so.cache:/etc/ld.so.cache \
+    -v /etc/ld.so.conf:/etc/ld.so.conf \
+    -v /etc/shadow:/etc/shadow \
+    -v /etc/passwd:/etc/passwd \
+    -v /etc/group:/etc/group \
+    -v /etc/localtime:/etc/localtime \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /tmp/.XIM-unix:/tmp/.XIM-unix \
+    -v /run/user/$(id -u)/bus:/run/user/$(id -u)/bus \
+    --mount type=bind,source=$HOME/Public,target=$HOME/Public \
+    --mount type=bind,source=$HOME/Downloads,target=$HOME/Downloads \
+    --mount type=bind,source=$HOME/Pictures,target=$HOME/Pictures \
+    --mount type=bind,source=$HOME/Music,target=$HOME/Music \
+    --mount type=bind,source=$HOME/Videos,target=$HOME/Videos \
+    --mount type=bind,source=$HOME/Documents,target=$HOME/Documents \
+    --mount type=bind,source=$HOME/Desktop,target=$HOME/Desktop \
+    -u $(id -u):$(id -g) \
+    -w /home/$(whoami) \
+    --entrypoint $(which nautilus) \
+    $extra_options \
+    ubuntu:20.04
+}
+
 function vfirefox() {
+  DOCKER="docker"
+  if [ "$(groups | grep docker)" == "" ]; then
+    DOCKER="sudo docker"
+  fi
+
   extra_options=""
   if [ "$FIREFOX_MEMORY_LIMIT" != "" ]; then
     extra_options="$extra_options --memory=$FIREFOX_MEMORY_LIMIT --oom-kill-disable"
@@ -448,19 +526,19 @@ function vfirefox() {
 
   config_volume_name="firefox-config-$(whoami)"
   cache_volume_name="firefox-cache-$(whoami)"
-  if [ "$(docker volume ls | awk '{print $2}' | grep $config_volume_name)" == "" ]; then
-    docker volume create $config_volume_name
-    docker run -it --rm -v $config_volume_name:/srv ubuntu:20.04 bash -c "chown -R $(id -u):$(id -g) /srv"
+  if [ "$($DOCKER volume ls | awk '{print $2}' | grep $config_volume_name)" == "" ]; then
+    $DOCKER volume create $config_volume_name
+    $DOCKER run -it --rm -v $config_volume_name:/srv ubuntu:20.04 bash -c "chown -R $(id -u):$(id -g) /srv"
   fi
-  if [ "$(docker volume ls | awk '{print $2}' | grep $cache_volume_name)" == "" ]; then
-    docker volume create $cache_volume_name
-    docker run -it --rm -v $cache_volume_name:/srv ubuntu:20.04 bash -c "chown -R $(id -u):$(id -g) /srv"
+  if [ "$($DOCKER volume ls | awk '{print $2}' | grep $cache_volume_name)" == "" ]; then
+    $DOCKER volume create $cache_volume_name
+    $DOCKER run -it --rm -v $cache_volume_name:/srv ubuntu:20.04 bash -c "chown -R $(id -u):$(id -g) /srv"
   fi
 
   pulse_socket_file="$HOME/.config/pulse/firefox.sock"
   pactl load-module module-native-protocol-unix socket=$pulse_socket_file
 
-  docker run \
+  $DOCKER run \
     -td \
     --name firefox \
     --rm \
