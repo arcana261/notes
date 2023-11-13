297ad4715a9deaef585739c893cce64b2af89476
Mohamadmehdi Kharatizadeh <info@ucoder.ir>
2023-11-13 21:12:57 +0000

    some notes i dont remember

---
+++ codes/decrypt.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo 'Mothers first name (all lower, like "sarah")?'
+read mother
+
+echo 'Fathers last name (all lower, like "john")?'
+read father
+
+echo 'First teachers name (all lower, like "nash")?'
+read teacher
+
+echo 'Cats name (all lower, hint: starts with "m", like "porter")?'
+read cat
+
+echo 'Birthday (YYYYmmdd format, like "20231121")?'
+read birthday
+
+echo 'Grandfathers first name (all lower, hint: starts with "hou")?'
+read grandfather
+
+echo 'Simple password (hint: dream)?'
+read simplepass
+
+password="$mother:$father:$teacher:$cat:$birthday:$grandfather:$simplepass"
+
+cat codes.txt | openssl aes-256-cbc -d -a -pass pass:$password > codes-unencrypted.txt
+
+
